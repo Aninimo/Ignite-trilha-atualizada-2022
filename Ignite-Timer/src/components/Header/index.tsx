83d3a3ec7fbff7523c4499614d4ed5dd7fbf4c94
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-09-28 09:54:09 -0300

    Create index.tsx

---
+++ Ignite-Timer/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from './styles'
+import { Scroll, Timer } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header(){
+  return(
+    <HeaderContainer>
+      <span>
+        <img src="https://raw.githubusercontent.com/rocketseat-education/02-ignite-timer/main/src/assets/logo-ignite.svg" alt="" />
+      </span>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
